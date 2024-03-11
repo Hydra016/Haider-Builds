92cd96144a919ed06140e3f7c7333e10616ab2de
Haider Mansoor <haider@bitfactory.io>
2024-03-11 23:04:55 +0200

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Link } from "react-scroll";
+import { Link as ScrollLink } from "react-scroll";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -17,38 +22,38 @@ const Footer = () => {
         </div>
         <div className="flex flex-col">
           <p className="text-2xl font-bold">Pages</p>
-          <Link
+          <ScrollLink
             smooth={true}
             duration={500}
             to="home"
             className="hover:text-secondary-text-color cursor-pointer"
           >
             Home
-          </Link>
-          <Link
+          </ScrollLink>
+          <ScrollLink
             smooth={true}
             duration={500}
             to="services"
             className="hover:text-secondary-text-color cursor-pointer"
           >
             Services
-          </Link>
-          <Link
+          </ScrollLink>
+          <ScrollLink
             smooth={true}
             duration={500}
             to="projects"
             className="hover:text-secondary-text-color cursor-pointer"
           >
             Projects
-          </Link>
-          <Link
+          </ScrollLink>
+          <ScrollLink
             smooth={true}
             duration={500}
             to="testimonials"
             className="hover:text-secondary-text-color cursor-pointer"
           >
             Testimonials
-          </Link>
+          </ScrollLink>
         </div>
         <div>
           <p className="text-2xl font-bold">Services</p>
@@ -59,10 +64,35 @@ const Footer = () => {
           <p>Website Maintenance</p>
           <p>Website Design</p>
         </div>
-        <div>
-          <p className="text-2xl font-bold">Based</p>
-          <p>Āzenes iela 8, Kurzemes rajons, Rīga, LV-1048</p>
-          <p>Made with 💚 by Haider</p>
+        <div className="flex flex-col justify-between">
+          <div>
+            <p className="text-2xl font-bold">Based</p>
+            <p>Āzenes iela 8, Kurzemes rajons, Rīga, LV-1048</p>
+            <p>Made with 💚 by Haider</p>
+          </div>
+          <div>
+            <p className="text-2xl font-bold">Socials</p>
+            <div className="flex">
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/haider-mansoor-4966a220b/"
+              >
+                <FaLinkedin className="mr-2" />
+              </Link>
+              <Link target="_blank" href="https://github.com/Hydra016">
+                <FaGithub className="mr-2" />
+              </Link>
+              <Link
+                target="_blank"
+                href="https://www.instagram.com/fifaholic247/"
+              >
+                <FaInstagram className="mr-2" />
+              </Link>
+              <Link target="_blank" href="https://twitter.com/HaiderMansoor8">
+                <FaXTwitter className="mr-2" />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex justify-center py-5 md:py-8 border-t-2 border-zinc-700">
