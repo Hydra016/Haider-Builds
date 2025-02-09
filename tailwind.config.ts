1ea63f01678ccf100a622cc1d6632ed499955309
HaiderPurple <haider@purplefire.io>
2025-02-09 22:29:47 +0200

    çhanges

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,11 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        "primary-color": "var(--primary-color)",
-        "secondary-color": "var(--secondary-color)",
-        "primary-text-color": "var(--primary-text-color)",
-        "secondary-text-color": "var(--secondary-text-color)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
